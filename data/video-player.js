18fbccd603c09376a2913b6b9b5602f9518cc5c5
Adam Van Ymeren <adam@vany.ca>
2017-06-15 15:52:52 -0400

    Fix embedded youtube video's auto playing in background.
    Something in the youtube's giant mess of code is responding to setting
    src = "".
    
    Fixes #77

--- data/video-player.js
+++ data/video-player.js
@@ -154,10 +154,8 @@ VP.prototype.clean = function() {
         if (this.player === vds[i])
             continue;
         vds[i].pause();
-        vds[i].src = "";
         vds[i].addEventListener("playing", (e) => {
             e.currentTarget.pause();
-            e.currentTarget.src = "";
         });
     }
     rmChildren(this.container);
